da4a32bbdc75fc15cec4ab73dc4d9f108e11b580
anita-jamgbadi <jamgbanita@gmail.com>
2023-03-01 22:41:39 +0000

    task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -31,6 +31,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		newLeftChild->left = parent->left;
 		newLeftChild->left->parent = newLeftChild;
 	}
+	else
+	{
+		newLeftChild->left = NULL;
+	}
 
 	parent->left = newLeftChild;
 

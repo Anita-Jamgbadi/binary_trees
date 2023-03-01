8725ff9c8d516ad6f0ff78821b2f82486551402f
anita-jamgbadi <jamgbanita@gmail.com>
2023-03-01 23:13:33 +0000

    task 5

--- binary_trees.h
+++ binary_trees.h
@@ -43,4 +43,6 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
 /* 4 */
 int binary_tree_is_leaf(const binary_tree_t *node);
+/* 5 */
+int binary_tree_is_root(const binary_tree_t *node);
 #endif

a810e606acbfc5ad0a84c6ffbae6fb10e2c7de66
anita-jamgbadi <jamgbanita@gmail.com>
2023-03-02 20:43:25 +0000

    task 13

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_nodes - counts nodes with at least 1 child in a binary tree
+ *
+ * @tree: root of tree to count on
+ * Return: size_t no of nodes fulfilling said condition or 0 if null
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	size_t count = 0;
+
+	if (tree->left != NULL || tree->right != NULL)
+		count = count + 1;
+
+	count += binary_tree_nodes(tree->left);
+	count += binary_tree_nodes(tree->right);
+
+	return (count);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -60,15 +60,27 @@ size_t binary_tree_size(const binary_tree_t *tree);
 /* 12 */
 size_t binary_tree_leaves(const binary_tree_t *tree);
 /* 13 */
+size_t binary_tree_nodes(const binary_tree_t *tree);
 /* 14 */
+int binary_tree_balance(const binary_tree_t *tree);
 /* 15 */
+int binary_tree_is_full(const binary_tree_t *tree);
 /* 16 */
+int binary_tree_is_perfect(const binary_tree_t *tree);
 /* 17 */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 /* 18 */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 /* 19 */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 /* 20 */
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 /* 21 */
+int binary_tree_is_complete(const binary_tree_t *tree);
 /* 22 */
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 /* 23 */
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 /* 24 */
+int binary_tree_is_bst(const binary_tree_t *tree);
 #endif

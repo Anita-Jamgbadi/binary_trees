da931e6bf5fa52a051c84e168c507f3ae5396b68
anita-jamgbadi <jamgbanita@gmail.com>
2023-03-02 23:17:25 +0000

    task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -19,7 +19,7 @@ int height(const binary_tree_t *tree)
 }
 /**
  * binary_tree_is_perfect - checks if a binary tree is perfect
- * perfect tree: when a tree is full and all its leaf nodes are at the same level
+ * perfect tree: tree is full and all its leaf nodes are at the same level
  *
  * @tree: root node
  * Return: 1 or 0
@@ -33,14 +33,15 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	l_height = height(tree->left);
 	r_height = height(tree->right);
-	
+
 	if (l_height == r_height)
 	{
 		if (tree->left == NULL || tree->right == NULL)
 			return (1);
 
 		else
-			return (binary_tree_is_perfect(tree->right) && binary_tree_is_perfect(tree->left));
+			return (binary_tree_is_perfect(tree->right) && binary_tree_is_perfect\
+					(tree->left));
 	}
 	else
 		return (0);
